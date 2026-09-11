d29d4610f9e2e32fa58505a336c7338bcbb4adbe
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2026-09-11 10:11:20 -0600

    Rename 'foods' to 'food' in index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const commandEmojis = {
     shared: '🧠',
     rashid: '🧞',
     imbuement: '🔥',
-    foods: '🧁',
+    food: '🧁',
 
     // menú
     menu: '💛',
@@ -54,7 +54,7 @@ const cooldownCommands = {
     monster: 3,
     rashid: 3,
     imbuement: 3,
-    foods: 3,
+    food: 3,
     shared: 3,
     media: 3,
     rguild: 3,
@@ -193,7 +193,7 @@ const sticker = require('./commands/sticker');
 const elfbot = require('./commands/elfbot');
 const client860 = require('./commands/client860');
 const helper = require('./commands/helper');
-const foods = require('./commands/foods');
+const food = require('./commands/food');
 const imbuement = require('./commands/imbuements');
 const media = require('./commands/media');
 const rguild = require('./commands/rguild');
@@ -224,7 +224,7 @@ const commands = {
     shared,
     rashid,
     imbuement,
-    foods,
+    food,
 
    // Rubinot
    rguild,
